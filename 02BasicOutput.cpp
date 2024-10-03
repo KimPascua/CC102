a85d185e66cd59f7699bbe53ed3c965729536cba
KimPascua <pascuaeyl@gmail.com>
2024-10-03 09:18:59 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Our Lady of Louredeas College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BSIT 1-A" << endl;
+	cout << "Kim Eyl Jane O. Pascua" << endl;
+	cout << "Hillcrest Subdivision Brgy. Bulhao Labo,Camarines Norte" << endl;
+	return 0;
+}
