111f005451d4630ae04789a99fc2d27ea0cc46a0
KimPascua <pascuaeyl@gmail.com>
2024-10-15 10:12:41 +0800

    Add files via upload

---
+++ CC102_PASCUAEXAM.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string name;
+   float sugarPriceUSD = 0.00, ricePricePound = 0.00,  sardinesPricePound = 0.00,  coffeePriceUSD = 0.00, milkPriceUSD = 0.00;
+   float sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD; 
+   float sugarQty, riceQty, sardinesQty, coffeeQty,  milkQty;
+   int total_cost
+   cout << "Enter the Quantity of Sugar: ";
+   cin >> sugarPriceUSD; 
+   cout << "Enter the Quantity of Rice: ";
+   cin >> ricePricePound;
+   cout << "Enter the Quantity of Sardines: ";
+   cin >> sardinesPricePound;
+   cout << "Enter the Quantity of Coffee: ";
+   cin >> coffeePriceUSD;
+   cout << "Enter the Quantity of Milk: ";
+   cin >> milkPriceUSD;
+   cout << "************************************************************************************" <<endl;
+   cout << "Enter Price of Sugar: ";
+   cin>> sugarQty;
+   cout << "Enter Price of Rice: ";
+   cin >> riceQty;
+   cout << "Enter Price of Sardines: ";
+   cin >> sardinesQty;
+   cout << "Enter Price of Coffee: ";
+   cin >> coffeeQty;
+   cout << "Enter Price of Milk: ";
+   cin >> milkQty;
+   cout << "************************************************************************************" <<endl;
+   return 0;
+   
+} 
